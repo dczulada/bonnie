a96cf5da982354994e86250e3f69d5d63f8acc63
David Czulada <dczulada@gmail.com>
2015-05-12 15:55:50 -0400

    added custom feature for MSRPOPL#

--- lib/measures/ade_helper.rb
+++ lib/measures/ade_helper.rb
@@ -3,8 +3,8 @@ module Measures
 
     ADE_SET_ID='A5E96A45-8132-4E72-BF4F-E8C81DB9E641'
 
-    ADE_FIELDS = {custom_functions: {OBSERV: 'hqmf.CustomCalc.ADE_TTR_OBSERV'},
-                  force_sources: ['LaboratoryTestResultInr']}
+    ADE_FIELDS = {custom_functions: {MSRPOPL: 'hqmf.CustomCalc.ADE_TTR_MSRPOPL',OBSERV: 'hqmf.CustomCalc.ADE_TTR_OBSERV'},
+                  force_sources: ['LaboratoryTestPerformedInr']}
 
     def self.update_if_ade(measure)
       if (measure.hqmf_set_id == ADE_SET_ID)
